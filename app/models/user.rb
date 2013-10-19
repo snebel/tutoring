1c820044dfa208134092414ed30d77c09224eb0e
snebel <sam@samnebel.com>
2013-10-19 00:26:23 -0400

    partials and edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
 
   private
 
-    def create_remember_token
-      self.remember_token = User.encrypt(User.new_remember_token)
-    end
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
