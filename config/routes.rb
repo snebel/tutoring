4b5c3f3b2a2d5736afffc52bfd799773b55f510e
snebel <sam@samnebel.com>
2013-10-22 12:59:13 -0400

    stripe badge

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Tutoring::Application.routes.draw do
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/resources', to: 'sessions#resources', via: 'get'
   match '/schedule', to: 'static_pages#schedule', via: 'get'
-  #match '/payments', to: 'charges#new', via: 'get'
+  match '/pay', to: 'charges#new', via: 'get'
   #match '/payments', to: 'charges#create', via: 'post'
   #match '/charges/new', to: 'charges#create', via: 'post'
 
